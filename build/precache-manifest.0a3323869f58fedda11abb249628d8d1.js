6c0f7cabffc9de17151decd1ca12531f9313132c
Abhidnyajoshi <abhidnyajoshi137@gmail.com>
2020-09-25 14:39:19 +0530

    Restapp Frontend

---
+++ build/precache-manifest.0a3323869f58fedda11abb249628d8d1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80741f522e7a88c06797245dbabcb8cc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "42f810072099fd0327b5",
+    "url": "/static/css/2.5c17b9f1.chunk.css"
+  },
+  {
+    "revision": "4672899ec796979d97fb",
+    "url": "/static/css/main.750f7610.chunk.css"
+  },
+  {
+    "revision": "42f810072099fd0327b5",
+    "url": "/static/js/2.82b01ee3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.82b01ee3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4672899ec796979d97fb",
+    "url": "/static/js/main.b616de24.chunk.js"
+  },
+  {
+    "revision": "0529a98c11a206755e39",
+    "url": "/static/js/runtime-main.99490353.js"
+  }
+]);
